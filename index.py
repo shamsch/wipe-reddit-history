e230ae90fd59e369d435dacf45242f05265b842d
Shamsur <shamsur314@gmail.com>
2023-07-17 22:08:54 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,39 +3,38 @@ import praw
 from datetime import datetime, timedelta
 import random
 
+REDDIT_APP_ID = os.getenv('REDDIT_APP_ID')
+REDDIT_APP_SECRET = os.getenv('REDDIT_APP_SECRET')
+REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
+REDDIT_PASSWORD = os.getenv('REDDIT_PASSWORD')
 
-def handler(event, context):
+def setup_reddit():
     reddit = praw.Reddit(
-        client_id=os.environ['REDDIT_APP_ID'],
-        client_secret=os.environ['REDDIT_APP_SECRET'],
-        username=os.environ['REDDIT_USERNAME'],
-        password=os.environ['REDDIT_PASSWORD'],
+        client_id=REDDIT_APP_ID,
+        client_secret=REDDIT_APP_SECRET,
+        username=REDDIT_USERNAME,
+        password=REDDIT_PASSWORD,
         user_agent="delete_comments"
     )
+    return reddit
 
-    cutoff_time = datetime.now() - timedelta(hours=72)
-
+def delete_comments(reddit):
+    cutoff_time_older = datetime.now() - timedelta(days=5)
+    cutoff_time_newer = datetime.now() - timedelta(days=3)
     deleted_comments_log = {}
 
-    for comment in reddit.redditor(os.environ['REDDIT_USERNAME']).comments.new(limit=None):
-        if datetime.fromtimestamp(comment.created_utc) < cutoff_time:
-            if comment.score < 1:
+    for comment in reddit.redditor(REDDIT_USERNAME).comments.new(limit=None):
+        comment_datetime = datetime.fromtimestamp(comment.created_utc)
+        if cutoff_time_older < comment_datetime < cutoff_time_newer:
+            if comment.score < 1 or (comment.score < 10 and random.random() < 0.50):
                 comment.delete()
                 deleted_comments_log[comment.id] = {
                     'timestamp': comment.created_utc,
                     'score': comment.score,
                     'body': comment.body
                 }
-            elif comment.score < 10 and random.random() < 0.50:
-                comment.delete()
-                deleted_comments_log[comment.id] = {
-                    'timestamp': comment.created_utc,
-                    'score': comment.score,
-                    'body': comment.body
-                }
-    return {
-        'statusCode': 200,
-        'username': os.environ['REDDIT_USERNAME'],
-        'body': deleted_comments_log,
-        'execution_time': f'UTC time: {datetime.now()}'
-    }
+    return deleted_comments_log
+
+def handler(event, context):
+    reddit = setup_reddit()
+    return deleted_comments_log
